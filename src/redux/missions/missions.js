4ddeaee0e9e6c0404cf6e4ac33ebb496bcd3450b
krisbobo <kiyongbohbo@gmail.com>
2023-01-11 21:55:39 +0100

    Add function to dispatch action on button click

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -3,6 +3,7 @@ import fetchMissions from './fetch';
 
 const GET_MISSIONS = 'Missions/Missions/GET_MISSIONS';
 const JOIN_MISSIONS = 'Missions/Missions/JOIN_MISSIONS';
+const EXIT_MISSIONS = 'Missions/Missions/EXIT_MISSIONS';
 
 const initialState = {
   missions: [],
@@ -39,6 +40,17 @@ export const joinMissions = createAsyncThunk(JOIN_MISSIONS, async (id) => {
       return mission;
     }
     return { ...mission, reserved: true };
+  });
+  return newState;
+});
+
+export const exitMissions = createAsyncThunk(EXIT_MISSIONS, async (id) => {
+  const data = await fetchMissions();
+  const newState = data.map((mission) => {
+    if (mission.id !== id) {
+      return mission;
+    }
+    return { ...mission, reserved: false };
   });
   return newState;
 });
