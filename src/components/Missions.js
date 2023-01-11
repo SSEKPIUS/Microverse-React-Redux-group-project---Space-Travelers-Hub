22f5d948e023b35f088f230bf42ff77dae1f6cca
krisbobo <kiyongbohbo@gmail.com>
2023-01-11 16:46:25 +0100

    Add function component to display fetched missions

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import TableContainer from '@material-ui/core/TableContainer';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import { getMissions } from '../redux/missions/missions';
 import style from './Missions.module.css';
 
 const Missions = () => {
-  const missions = [];
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions.missions);
+  useEffect(() => {
+    dispatch(getMissions());
+  }, [dispatch]);
 
   return (
     <TableContainer>
@@ -26,8 +32,8 @@ const Missions = () => {
             <TableRow key={mission.id}>
               <TableCell className={style.missionName}>{mission.mission_name}</TableCell>
               <TableCell className={style.descriptionText}>{mission.description}</TableCell>
-              <TableCell className={style.missionStatus}><button disabled type="button" className="statusBtn">{mission.status}</button></TableCell>
-              <TableCell className={style.membership}><button type="button" className="membershipBtn">{mission.membership}</button></TableCell>
+              <TableCell className={style.missionStatus}><button disabled type="button" className="statusBtn">NOT A MEMBER</button></TableCell>
+              <TableCell className={style.membership}><button type="button" className="membershipBtn">Join Mission</button></TableCell>
             </TableRow>
           ))}
         </TableBody>
