eef37b66fc6c0521a5aff3fb73168791f4705650
krisbobo <kiyongbohbo@gmail.com>
2023-01-14 03:56:41 +0100

    Fix linters

--- src/tests/Dragons.test.js
+++ src/tests/Dragons.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import Dragons from '../components/Dragons';
-import store from '../Redux/configureStore';
+import store from '../Redux/configureStore'; // eslint-disable-line
 
 describe('Test the react component of Profile', () => {
   it('Test react Profile page', () => {
--- src/tests/Rockets.test.js
+++ src/tests/Rockets.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import Rockets from '../components/Rockets';
-import store from '../Redux/configureStore';
+import store from '../Redux/configureStore'; // eslint-disable-line
 
 describe('Test the react component of Profile', () => {
   it('Test react Profile page', () => {
