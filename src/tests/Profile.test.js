37f26510fae09658f433b40a857e55c33563e61d
pius ssekweyama <ssekweyamapius@gmail.com>
2023-01-14 11:44:08 +0300

    fix tests

--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import Profile from '../components/Profile';
-import store from '../Redux/configureStore'; // eslint-disable-line
+import store from '../redux/configureStore';
 
 describe('Test the react component of Profile', () => {
   it('Test react Profile page', () => {
