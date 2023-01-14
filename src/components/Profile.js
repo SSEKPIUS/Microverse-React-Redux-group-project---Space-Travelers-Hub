5a6478906efb90df0b0d2dbfee4b757e2e0bff24
pius ssekweyama <ssekweyamapius@gmail.com>
2023-01-14 10:40:38 +0300

    fix linters

--- src/components/Profile.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Profile = () => (
-  <>
-    profile
-  </>
-);
-
-export default Profile;
--- src/redux/configureStore.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import rocketReducer from './rockets/rockets';
-
-const store = configureStore({
-  reducer: {
-    rockets: rocketReducer,
-  },
-});
-export default store;
